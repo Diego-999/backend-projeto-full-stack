1ca1a9fac35491bcc72cb057fc4254b6e38ce8d7
Diego <di__e__go@hotmail.com>
2020-09-23 22:12:22 -0300

    ajustes 01

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import { AddressInfo } from "net";
 import { signUp } from "./controller/Signup";
 import { login } from "./controller/Login";
 import { ImageController } from "./controller/ImageController";
+import * as cors from "cors";
 
 const app = express();
 const imageController = new ImageController();
 app.use(express.json());
 dotenv.config();
+app.use(cors());
 
 app.post("/signup", signUp);
 app.post("/login", login);
@@ -16,7 +18,7 @@ app.post("/image", imageController.createImage);
 app.get("/image", imageController.getImageByProfile);
 app.get("/image/:id", imageController.getImageById);
 
-const server = app.listen(3000, () => {
+const server = app.listen(4000, () => {
   if (server) {
     const address = server.address() as AddressInfo;
     console.log(`Server is running in http://localhost: ${address.port}`);
